e838830cbc3ea2265b0b2cd86eaafe921bc05aaf
Heidi Joutsijoki <heidi20151@student.hamk.fi>
2023-11-10 11:16:49 +0200

    Get, post, delete, put works

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-
-app.use(express.json()) // json-parser, middleware
+const Note = require('./models/note')
+const cors = require('cors')
 
 // oma middleware:
 const requestLogger = (request, response, next) => {
@@ -12,49 +13,50 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(requestLogger)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-const cors = require('cors')
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
 
-app.use(cors())
-
-app.use(express.static('dist'))
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy and fun",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-]
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(requestLogger)
+app.use(express.json()) // json-parser, middleware
+app.use(cors())
+app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end() // end = pyyntöön vastataan ilman dataa
-  }
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+
+    .catch(error => next(error)) // parametri siirtyy virheenkäsittelijämiddlewarelle
 })
 
 const generateId = () => {
@@ -64,40 +66,51 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
 
-  response.json(note)
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+app.put('/api/notes/:id', (request, response, next) => {
 
-app.use(unknownEndpoint)
+  const { content, important } = request.body
 
-const PORT = process.env.PORT || 3001
+  Note.findByIdAndUpdate(
+    request.params.id, 
+
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint) // Tää myös tärkee olla vasta getin jälkeen
+app.use(errorHandler) // TÄÄ TULEE AINA REKISTERÖIDÄ MUIDEN JÄLKEEN!
+// Middlewaret  suoritetaan siinä järjestyksessä, missä ne on otettu käyttöön funktiolla app.use
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
